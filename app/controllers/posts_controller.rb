147310c109d91d3f6e3450d8153d3dcb6959605f
🏄  Jeremy Cohen Hoffing <jeremy@goodinc.com>
2013-08-29 17:35:06 -0700

    Fix indentation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,10 @@
 class PostsController < ApplicationController
-	def index
-		@posts = KPCC.get_all_posts
-	end
+  def index
+    @posts = KPCC.get_all_posts
+  end
 
-	def filter
-		@filtered_posts = KPCC.filter(params)
-	end
+  def filter
+    @filtered_posts = KPCC.filter(params)
+  end
 
-	def show
-	end
 end
